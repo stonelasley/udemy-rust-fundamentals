4634fd53c9de7670e31485f41929e91aeac950b7
Stone C. Lasley <stonelasley@gmail.com>
2022-05-30 21:13:39 -0600

    section(three): mars calculator

---
+++ mars_calc/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    println!("Enter your weight (kg): ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+    println!("Weight: {}", weight);
+
+    let mars_weight = calculate_weight_on_mars(100.0);
+    println!("Weight on Mars: {}kg", mars_weight);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    return (weight / 9.81) * 3.711;
+}
